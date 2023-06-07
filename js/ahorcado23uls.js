01b875b8090cf350f0159f76f7fa6b5c252001b2
Abssel <49079340+Abssel@users.noreply.github.com>
2023-06-07 17:29:15 +0000

    CREANDO LAS VARIABLES

--- js/ahorcado23uls.js
+++ js/ahorcado23uls.js
@@ -1,1 +1,25 @@
-console. log("Hello world!");
+var words = {
+    "tema1": ["javascript", "programacion", "desarrollo"],
+    "tema2": ["ahorcado", "palabra", "adivinar"]
+};
+var palabra_actual = '';
+var  letras_adivinadas=[];
+
+
+var Intentos_restantes = 6;
+
+
+var puntaje = 0;
+
+var nombreJugador =" ";
+
+var playerContainer = document.getElementById('player-container');
+var playerInput = document.getElementById('player-input');
+var gameContainer = document.getElementById('game-container');
+var wordContainer = document.getElementById('word');
+var guessInput = document.getElementById('guess-input');
+var resultContainer = document.getElementById('result-container');
+var resultMessage = document.getElementById('result');
+
+
+
