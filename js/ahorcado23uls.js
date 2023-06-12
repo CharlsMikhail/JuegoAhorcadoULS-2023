6d73d496b430df1024d13934630811a0d649fb47
Abssel <49079340+Abssel@users.noreply.github.com>
2023-06-12 15:40:24 +0000

    Creando los botones

--- js/ahorcado23uls.js
+++ js/ahorcado23uls.js
@@ -18,3 +18,35 @@ const obtenerPreguntas = tema => {
   const preguntaAleatoria = preguntas [Math.floor(Math.random()*preguntas.length)];
   return preguntaAleatoria;
 };
+
+document.addEventListener('DOMContentLoadedD',()=>{
+  const wordDiplay= document.getElementById('word-diplay');
+  const guessesDisplay= document.getElementById('guesses-display');
+  const guessInput = document.getElementById('guess-input');
+  const guessButton  = document.getElementById('guess-button');
+  
+
+  let palabraOculta='';
+  let puntuacion =0;
+
+
+  const temas = cargarTemas();
+  const temaIndex = prompt('Temas disponibles:\n${temas.map((tema, index) => `${index + 1}. ${tema}`).join('\n')}\n\nElige un número de tema:');
+  const temaElegido = tema[temaIndex - 1];
+  const Pregunta = obtenerPreguntas(temaElegido);
+
+
+
+  for (let i =0; i< Pregunta.length; i++){
+    if (Pregunta[i]===' '){
+      palabraOculta += ' ';
+
+    } else {
+      palabraOculta += ' ';
+    }
+  }
+  wordDiplay.textContent=palabraOculta;
+  guessButton.addEventListener('click',()=>{
+    const letra = guessInput
+  })
+})
